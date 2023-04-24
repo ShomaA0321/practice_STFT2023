92f9abca420baecefc0de7ee21c0d7c7b785172a
keis98 <130130653+keis98@users.noreply.github.com>
2023-04-24 17:15:55 +0900

    Update step2.m

--- step2.m
+++ step2.m
@@ -3,11 +3,11 @@ clear; clc; close all;
 sigLen = 441000;
 shiftLen = 1024;
 winLen = 2048;
-frameLen = %frameLenを計算
+frameLen = ceil((sigLen-winLen)/shiftLen + 1);
 
 % 適切なサイズの0行列を用意します
 % 床関数の追加
-Spec = zeros(winLen/2+1, frameLen); 
+Spec = zeros(floor(winLen/2)+1, frameLen); 
 
 % forループを用いてDFTmodoki(M)をL回実行，各実行結果をSpecの各列に代入します
 for i = 1:frameLen
